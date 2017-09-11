6ac768b6ed7c157c1f78f02898af5f8ed4f469c0
grahamstark <graham.stark@virtual-worlds.biz>
2017-09-11 11:41:30 +0100

    some way to parsing popn files

--- model/scripts/parse_forecast_files.rb
+++ model/scripts/parse_forecast_files.rb
@@ -1,18 +1,55 @@
 #!/usr/bin/ruby
 require 'csv'
 require 'utils'
-require 'narray'
-require 'set'
+# require 'narray'
+# require 'set'
 
 DATA_PATH="/home/graham_s/VirtualWorlds/projects/scottish_child_poverty_projections/docs/"
 
 def toCSV( filename )
         f = File.new( filename, 'rb' );
-        lines = CSV.parse( f )
+        lines = CSV.parse( f, { col_sep:"\t" } )
         f.close()
         return lines
 end
 
-lines = toCSV( "#{DATA_PATH}/nrs/pp-2014-based-add-var-euro-zeroumig-scotland-syoa-1.tab" );
+def readBlock( lines, pos )
+       label = lines[pos][0]
+       puts "label #{label}\n"
+       pos += 2
+       puts "on line #{pos}\n"
+       p lines[pos]
+       years = []
+       yearsStr = lines[pos][1..-1]
+       yearsStr.each{
+               |ystr|
+                years << ystr.to_i       
+       }
+       pos += 2
+       targetGroup = lines[pos][0]
+       puts "target group #{targetGroup}\n"
+       begin
+               pos +=1
+               puts "on line #{pos} |#{lines[pos][0]}|\n"
+       end while ! lines[pos][1].nil?
+       return pos if lines[pos+1][0].nil?
+       -1
+end
 
-p lines
+# fname = ARGS[0]
+fname = "#{DATA_PATH}/#{ARGV[0]}"
+variant = ARGV[1]
+## nrs/pp-2014-based-add-var-euro-zeroeumig-scotland-syoa-1.tab
+puts "opening #{fname}\n"
+lines = toCSV( fname );
+pos = 0
+# p lines
+variant_desc = lines[0][0]
+
+begin
+        pos = readBlock( lines, pos )
+end while pos != -1
+
+
+
+
