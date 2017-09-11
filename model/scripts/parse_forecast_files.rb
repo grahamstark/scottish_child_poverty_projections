ba15c058b09e66509f5cca17b644bf6650e81679
grahamstark <graham.stark@virtual-worlds.biz>
2017-09-11 15:27:00 +0100

    more popn parsing

--- model/scripts/parse_forecast_files.rb
+++ model/scripts/parse_forecast_files.rb
@@ -16,9 +16,11 @@ end
 def readBlock( lines, pos )
        label = lines[pos][0]
        puts "label #{label}\n"
-       pos += 2
+       pos += 1
+       pos += 1 if lines[pos][1].nil? # check for irregular spacing
        puts "on line #{pos}\n"
        p lines[pos]
+       data = []
        years = []
        yearsStr = lines[pos][1..-1]
        yearsStr.each{
@@ -27,13 +29,20 @@ def readBlock( lines, pos )
        }
        pos += 2
        targetGroup = lines[pos][0]
+       pos +=1
+       p years      
        puts "target group #{targetGroup}\n"
        begin
+               
+               key = lines[pos][0]
+               key = "age_#{key}" if key =~ /\d+/
+               key = censor("#{targetGroup}_#{key}")
+               puts "on line #{pos} |#{lines[pos][0]}| key #{key}\n"
                pos +=1
-               puts "on line #{pos} |#{lines[pos][0]}|\n"
+               
        end while ! lines[pos][1].nil?
-       return pos if lines[pos+1][0].nil?
-       -1
+       return pos+1
+       
 end
 
 # fname = ARGS[0]
@@ -48,7 +57,9 @@ variant_desc = lines[0][0]
 
 begin
         pos = readBlock( lines, pos )
-end while pos != -1
+        puts "pos end #{pos}\n"
+        
+end while pos < lines.length-1
 
 
 
