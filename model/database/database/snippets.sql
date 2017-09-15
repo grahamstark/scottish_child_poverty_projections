21a8428a2fda189c4b1afa1122b87985cddb29d9
Graham Stark <graham.stark@virtual-worlds.biz>
2017-09-15 11:00:49 +0100

    various load files added

---
+++ model/database/database/snippets.sql
@@ -0,0 +1,4 @@
+
+set search_path=public,target_data,frs;
+
+select * from target_data.forecast_variant;
---
+++ model/scripts/load_all_forecasts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# individual years forecast, zero eu migration, scotland only
+./parse_forecast_files.rb  pp-2014-based-add-var-euro-zeroeumig-scotland-syoa-1.tab zeroeu SCO 2014 NRS
+
