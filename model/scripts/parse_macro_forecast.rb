06b7559acef29992266768b5148a583964b77988
Graham Stark <graham.stark@virtual-worlds.biz>
2017-09-18 23:57:31 +0100

    macro stuff, inc. jamming '_age_16_plus' in headers for tab 1.5

--- model/scripts/parse_macro_forecast.rb
+++ model/scripts/parse_macro_forecast.rb
@@ -4,8 +4,67 @@ require 'utils'
 require 'sequel'
 require './forecast_libs'
 
-def readMacro( lines, startCol, startKey, out )
+def readMacro( lines, startCol, keyLineStart, out )
+        p = 0
+        keys = []
+        data = {}
+        years = []
+        loop do
+              p lines[p]
+              if(( ! lines[p][startCol].nil? ) && ( lines[p][startCol] =~ /#{keyLineStart}/ ))
+                        lines[p][startCol..-1].each{
+                                |l|
+                                puts "p = #{p} l '#{l}'\n"
+                                if l =~ /(.*)\(.*/
+                                        l = $1
+                                end
+                                l.gsub!( 'billion', '' )
+                                key = censor( l )
+                                # horrible hack ..
+                                keys << key
+                        }
+                        puts "made keys as #{out[:keys]}; breaking"
+                        break;
+              end
+              p += 1
+        end
+        loop do
+                puts "lines[#{p}][1] #{lines[p][1]}\n"
+                break if lines[p][1] =~ /\//
+                p += 1
+        end
         
+        puts "starting on line #{p}"
+        begin
+                year = lines[p][1].to_i
+                years << year
+                puts "on line #{p}; year #{year}\n"
+                c = 0
+                lines[p][startCol..-1].each{
+                        |d|
+                        key = keys[c]
+                        data[key] = [] if data[key].nil?
+                        data[key] << d.to_f 
+                        c += 1
+                        
+                        
+
+                }
+                p += 1
+        end while (lines[p][1] =~ /[0-9\/]+/)
+        p keys
+        p years
+        p data
+        data.each{
+                |k,v|
+                out[:data][k] = v;
+        }
+        out[:years] = years # if out[:years].nil?
+        keys.each{
+                |k|
+                out[:keys] << k
+        }
+
         return out     
 end
 
@@ -15,6 +74,16 @@ source = 'obr'
         |which|
         fullFName = "#{DATA_PATH}/#{source}/obr_forecast_#{which}.tab"
         puts fullFName;
+        lines = toCSV( fullFName );
+        case which
+        when 'gdp_per_capita' then
+                        out = readMacro( lines, 2, 'LFS', out )
+        when 'prices' then                  
+                        out = readMacro( lines, 11, 'RPI', out )
+        when 'employment' then              
+                        out = readMacro( lines, 2, 'Employment', out )
+        end
         
-
 }
+
+p out
