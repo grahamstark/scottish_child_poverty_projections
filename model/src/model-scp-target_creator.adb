283cf10918b71117cbeb53d773504b6e9fbb4c65
Graham Stark <graham.stark@virtual-worlds.biz>
2017-09-20 22:07:50 +0100

    fields added to run to select macro, etc. stuff correctly

--- model/src/model-scp-target_creator.adb
+++ model/src/model-scp-target_creator.adb
@@ -7,6 +7,7 @@ with Ada.Text_IO;
 
 with Data_Constants;
 with Base_Model_Types;
+with Text_Utils;
 
 with GNATColl.Traces;
 with GNATCOLL.SQL.Exec;
@@ -14,6 +15,7 @@ with GNATCOLL.SQL.Exec;
 with Ukds.Target_Data.Households_Forecasts_IO;
 with Ukds.Target_Data.Run_IO;
 with Ukds.Target_Data.Macro_Forecasts_IO;
+with Ukds.Target_Data.Households_Forecasts_IO;
 with Ukds.Target_Data.Population_Forecasts_IO;
 with Ukds.Target_Data.Target_Dataset_IO;
 
@@ -26,7 +28,7 @@ package body Model.SCP.Target_Creator is
 
    use UKDS.Target_Data;
    use UKDS;
-   
+   use Text_Utils;   
    use Ada.Assertions;
    use Ada.Text_IO;
    use Ada.Calendar;
@@ -36,11 +38,59 @@ package body Model.SCP.Target_Creator is
    package d renames DB_Commons;
    
    procedure Create_Dataset( the_run : Run ) is
+      pop_crit : d.Criteria;
+      mac_crit : d.Criteria;
+      hh_crit : d.Criteria;
    begin
-      
-      
-      
-      null;
+      -- Population_Forecasts_IO.Add_Variant( pop_crit, the_run.people_source ); 
+      -- Households_Forecasts_IO.Add_Variant( hh_crit, the_run.household_source );
+      -- Macro_Forecasts_IO.Add_Variant( mac_crit, the_run.macro_source );
+      for year in the_run.start_year .. the_run.end_year loop
+         
+         declare
+            female_popn : constant Population_Forecasts := Population_Forecasts_IO.Retrieve_By_PK(
+               year => year,
+               rec_type => TuS( "persons" ),
+               variant  => the_run.population_source,
+               country  => the_run.country,
+               edition  => the_run.population_edition,
+               target_group => TuS( "FEMALES" )                           
+            );
+            male_popn : constant Population_Forecasts := Population_Forecasts_IO.Retrieve_By_PK(
+               year => year,
+               rec_type => TuS( "persons" ),
+               variant  => the_run.population_source,
+               country  => the_run.country,
+               edition  => the_run.population_edition,
+               target_group => TuS( "MALES" )                           
+            );
+            households : constant Households_Forecasts := Households_Forecasts_IO.Retrieve_By_PK(
+               year => year,
+               rec_type => TuS( "households" ),
+               variant  => the_run.households_source,
+               country  => the_run.country,
+               edition  => the_run.households_edition                   
+            );
+            macro : constant Macro_Forecasts := Macro_Forecasts_IO.Retrieve_By_PK(
+               year => year,
+               rec_type => TuS( "macro" ),
+               variant  => the_run.macro_source,
+               country  => the_run.country,
+               edition  => the_run.macro_edition                   
+            ); 
+         begin
+            null;
+         end;
+         -- Population_Forecasts_IO.Add_Year( pop_crit, year ); 
+         -- Households_Forecasts_IO.Add_Year( hh_crit, year );
+         -- Macro_Forecasts_IO.Add_Year( mac_crit, year );
+         -- 
+         -- 
+         -- 
+         -- d.Remove_From_Criteria( mac_crit, "year" );
+         -- d.Remove_From_Criteria( pop_crit, "year" );
+         -- d.Remove_From_Criteria( hh_crit, "year" );
+      end loop;
    end Create_Dataset;
 
 end Model.SCP.Target_Creator;
