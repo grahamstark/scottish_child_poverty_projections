ea3c94f731334f98b1818110767610e9b06f8836
Graham Stark <graham.stark@virtual-worlds.biz>
2017-09-21 16:27:49 +0100

    replaced amount_arrays with vectors

--- model/drivers/basic_scp_driver.adb
+++ model/drivers/basic_scp_driver.adb
@@ -11,6 +11,7 @@ with Ada.Text_IO;
 with Ada.Exceptions;
 with Ada.Strings.Unbounded; 
 with Text_Utils;
+with Weighting_Commons;
 
 with UKDS.Target_Data;
 with UKDS.Target_Data.Run_IO;
@@ -23,6 +24,7 @@ procedure Basic_SCP_Driver is
    use Ada.Calendar;
    use Text_Utils;
    use SCP_Types;
+   use Weighting_Commons;
    
    startTime    : Time;
    endTime      : Time;
@@ -32,20 +34,19 @@ procedure Basic_SCP_Driver is
 begin
    startTime := Clock;
    the_run.run_type := run_type;
+   the_run.user_id := 1;
    Put_Line( "We're making a start on this.." );
    case run_type is
    when data_generation =>      
       the_run.start_year := 2008;
       the_run.end_year := 2015;
       the_run.run_id := 999_998;
-      the_run.user_id := 1;
       UKDS.Target_Data.Run_IO.Save( the_run );
       Model.SCP.FRS_Creator.Create_Dataset( the_run );
    when target_generation =>
       the_run.start_year := 2014;
       the_run.end_year := 2021;
       the_run.run_id := 100_002;
-      the_run.user_id := 1;
       
       the_run.households_variant := TuS( "pph" );
       the_run.households_edition := 2014;
@@ -57,9 +58,26 @@ begin
       UKDS.Target_Data.Run_IO.Save( the_run );
       Model.SCP.Target_Creator.Create_Dataset( the_run );
    when weights_generation =>
+      the_run.start_year := 2014;
+      the_run.end_year := 2021;
+      the_run.run_id := 200_000;
+
+      the_run.weighting_function := constrained_chi_square;
+      the_run.weighting_lower_bound := 0.2;
+      the_run.weighting_upper_bound := 2.0;
+      the_run.targets_run_id := 100_000;      
+      the_run.targets_run_user_id := 1;
+      the_run.data_run_id := 999_998;
+      the_run.data_run_user_id := 1;
+      the_run.selected_clauses := ( others => false );
+      the_run.country := TuS( "SCO" );
+      
+      UKDS.Target_Data.Run_IO.Save( the_run );
+      
       null;
    when validation =>
-      null;
+      
+      UKDS.Target_Data.Run_IO.Save( the_run );
    end case;
    endTime := Clock;
    elapsed := endTime - startTime;
