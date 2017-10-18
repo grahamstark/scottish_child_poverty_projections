04cf2805cd6fad3e9db8cdcb4a9a33ac0aa1af9e
Graham Stark <graham.stark@virtual-worlds.biz>
2017-10-18 20:16:06 +0100

    1/2 way through adding participation and different hhld records

---
+++ model/src/scp_types.adb
@@ -0,0 +1,9 @@
+package body SCP_Types is
+   
+   function Image( c : Countries ) return Unbounded_String is
+      s : constant String := Countries'Image( c );
+   begin
+      return To_Unbounded_String( s( s'First+1 .. s'Last ));
+   end Image;
+
+end SCP_Types;
