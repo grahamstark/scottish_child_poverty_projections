f5be7d40747a1f548a58c190ae2e8b9e0e2dc4fa
Graham Stark <graham.stark@virtual-worlds.biz>
2018-01-02 22:17:00 +0000

    OBR Nov 2017 stuff continued

--- model/scripts/create_2016_targets.sh
+++ model/scripts/create_2016_targets.sh
@@ -1,3 +1,6 @@
+#
+# Scotland ones
+#
 bin/basic_scp_driver -r 100120
 bin/basic_scp_driver -r 100121
 bin/basic_scp_driver -r 100122
@@ -11,3 +14,18 @@ bin/basic_scp_driver -r 100129
 bin/basic_scp_driver -r 100130
 bin/basic_scp_driver -r 100131
 bin/basic_scp_driver -r 100132
+#
+# UK wide ones
+#
+bin/basic_scp_driver -r 100140
+bin/basic_scp_driver -r 100141
+bin/basic_scp_driver -r 100142
+bin/basic_scp_driver -r 100143
+bin/basic_scp_driver -r 100144
+bin/basic_scp_driver -r 100145
+bin/basic_scp_driver -r 100146
+bin/basic_scp_driver -r 100147
+bin/basic_scp_driver -r 100148
+bin/basic_scp_driver -r 100149
+
+
